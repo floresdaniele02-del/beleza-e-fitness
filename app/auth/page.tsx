7b4afa8493bb093bbe62a313e211da9a8b6b5781
floresdaniele02-del <floresdaniele02@gmail.com>
2026-02-03 15:37:45 -0300

    Cria rota /auth

---
+++ app/auth/page.tsx
@@ -0,0 +1,8 @@
+export default function AuthPage() {
+  return (
+    <main style={{padding: 40, textAlign: "center"}}>
+      <h1>🔐 Autorização recebida</h1>
+      <p>Você pode fechar esta página.</p>
+    </main>
+  );
+}
