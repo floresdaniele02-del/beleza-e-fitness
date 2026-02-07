84e86377a08e0b1580dd2ba86101c15942ce3bdc
floresdaniele02-del <floresdaniele02@gmail.com>
2026-02-07 17:25:12 -0300

    Troca code por token ML

--- app/api/mercadolivre/token/route.ts
+++ app/api/mercadolivre/token/route.ts
@@ -1,21 +1,18 @@
-import { NextResponse } from "next/server";
+imimport { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const code = searchParams.get("code");
-
-  const response = await fetch("https://api.mercadolibre.com/oauth/token", {
+export async function GET() {
+  const res = await fetch("https://api.mercadolibre.com/oauth/token", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       grant_type: "authorization_code",
-      client_id: process.env.ML_APP_ID,
-      client_secret: process.env.ML_SECRET_KEY,
-      code,
+      client_id: process.env.ML_CLIENT_ID,
+      client_secret: process.env.ML_CLIENT_SECRET,
+      code: "TG-69879d7691e8ef00014ac1a7-630548155",
       redirect_uri: "https://beleza-e-fitness.vercel.app/auth"
-    })
+    }),
   });
 
-  const data = await response.json();
+  const data = await res.json();
   return NextResponse.json(data);
 }
