8e51b2b117ae46e8753a2e7123e48046dd100e1a
floresdaniele02-del <floresdaniele02@gmail.com>
2026-02-07 17:32:17 -0300

    novo code

--- app/api/mercadolivre/token/route.ts
+++ app/api/mercadolivre/token/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
       grant_type: "authorization_code",
       client_id: process.env.ML_CLIENT_ID,
       client_secret: process.env.ML_CLIENT_SECRET,
-      code: "TG-69879d7691e8ef00014ac1a7-630548155",
+      code: "TG-6987a110a6465c00011d89e8-630548155",
       redirect_uri: "https://beleza-e-fitness.vercel.app/auth"
     }),
   });
